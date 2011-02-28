ba8995cf075253a4c69132390d0f1bb6e97723fc
wynner <wynner@2c99d0e6-60b6-42c7-aff0-cedb7f4b1d2e>
2011-02-28 21:56:02 +0000

    Test commit.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/ncivalueset/ncivalueseteditor/trunk@56 2c99d0e6-60b6-42c7-aff0-cedb7f4b1d2e

--- software/ncivalueseteditor/src/java/gov/nih/nci/evs/valueseteditor/junits/TestSearchUtil.java
+++ software/ncivalueseteditor/src/java/gov/nih/nci/evs/valueseteditor/junits/TestSearchUtil.java
@@ -51,7 +51,7 @@ import junit.framework.TestCase;
 public class TestSearchUtil extends TestCase {
 
 	  public void testAdd() {
-	        int num1 = 3;
+	        int num1 = 3; //Test commit
 	        int num2 = 2;
 	        int total = 5;
 	        int sum = 0;
